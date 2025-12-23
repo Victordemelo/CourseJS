623e11508d6c01e38b476714255ded1eeef0b74c
victor <victorgameplays535@gmail.com>
2025-12-23 09:11:32 -0300

    Init: Resolvendo as aulas sobre comentários

---
+++ aula03/comentarios.js
@@ -0,0 +1,12 @@
+// Para comentar basta comolocar duas barras
+console.log("teste de texto");
+console.log("Linha 1");
+console.log("Linha 2");
+console.log("Linha 3");
+console.log("Linha 4");
+//Vai ser ignorado
+
+
+/* codigo de bloco
+teste de bloco */
+console.log("teste de bloco")
