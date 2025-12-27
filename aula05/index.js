29b8dd8202031387bebd6c089221e9a55560bad9
victor <victorgameplays535@gmail.com>
2025-12-27 15:55:12 -0300

    Init: Conhecendo as variaveis de JS

---
+++ aula05/index.js
@@ -0,0 +1,12 @@
+var teste = 0;
+let teste2 = 1;
+
+let nome = "João";
+
+console.log(nome, "Nasceu em 1984");
+console.log(nome, "Em 2002 conheceu maria");
+console.log(nome, "se casou com maria em 2012");
+console.log("Maria teve 1 filho com", nome, "em 2015");
+console.log("O filho de", nome, "se chama eduardo");
+
+//SEMPRE UTILIZAR O LET E NÃO VAR
